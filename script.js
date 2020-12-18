e4dca9a5f142e9ef020a55b431dd19df5fc69e38
Anna Blankell <anna.blankell@outlook.com>
2020-12-18 10:58:51 +0100

    added number in display

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const calculator = document.querySelector(".calculator");
   const keys = calculator.querySelector(".calculator__keys");
+  const calcDisplay = document.querySelector(".calculator__display");
+  let numberArray = [];
 
   keys.addEventListener("click", (e) => {
     if (e.target.matches("button")) {
-      // Do something
+      //   let ourNumber = calcDisplay.textContent;
+      //   ourNumber = Number(e.target.textContent);
+
+      let ourNumber = Number(e.target.textContent);
+      calcDisplay.textContent = ourNumber;
     }
   });
 });
