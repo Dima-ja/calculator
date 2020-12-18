a49abad1fdf29ba413b2acf5bd6bfbca690202d8
angiescript <johansson.angelika@hotmail.com>
2020-12-18 14:39:50 +0100

    cleaned code

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const calculator = document.querySelector(".calculator");
   const keys = calculator.querySelector(".calculator__keys");
   const calcDisplay = document.querySelector(".calculator__display");
-  //let currentNumbers = [];
-  let firstNumber="";
-  let savedfirst="";
-  let secondNumber ="";
+
+  let totalNumber="";
+  let savedFirst="";
   let operatorValue = "";
   let sum = 0;
 
   keys.addEventListener("click", (e) => {
     if (e.target.matches(".clear")) {
-      firstNumber="";
-      savedfirst="";
-      secondNumber ="";
+      totalNumber="";
+      savedFirst="";
       operatorValue = "";
       sum = 0;
       calcDisplay.textContent="0";
@@ -21,41 +19,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (e.target.matches(".noBtn")) {
       let currentNumber = e.target.textContent;
-      firstNumber += currentNumber;
-      calcDisplay.textContent = firstNumber;
-      return firstNumber;
-
+      totalNumber += currentNumber;
+      calcDisplay.textContent = totalNumber;
+      return totalNumber;
     } else if (e.target.matches(".key--equal")) {
-        let numbersavedfirst = Number(savedfirst);
-        let numberfirstnumber = Number(firstNumber);
+        let intFirst = Number(savedFirst);
+        let intSecond = Number(totalNumber);
         
         switch(operatorValue) {
           case "+":
-            sum = numbersavedfirst + numberfirstnumber;
+            sum = intFirst + intSecond;
             break;
           case "-":
-            sum = numbersavedfirst - numberfirstnumber;
+            sum = intFirst - intSecond;
             break;
           case "÷":
-            sum = numbersavedfirst/numberfirstnumber;
+            sum = intFirst/intSecond;
             break;
           case "×":
-            sum = numbersavedfirst*numberfirstnumber
+            sum = intFirst*intSecond;
             break;
         }
-        console.log(sum);
         calcDisplay.textContent = sum;
-        firstNumber = sum;
+        totalNumber = sum;
 
     } else if (e.target.matches(".key--operator")) {
         operatorValue = e.target.textContent;
-        //keys.querySelectorAll('.noBtn').classList.add('noBtn2');
-        //keys.querySelectorAll('.noBtn').classList.remove('.noBtn')
-        console.log(operatorValue);
-
-        savedfirst = firstNumber;
-        firstNumber = "";
-
+        savedFirst = totalNumber;
+        totalNumber = "";
     };
 
   });
